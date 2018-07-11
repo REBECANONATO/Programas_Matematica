7049fc31ef1ce4b7a1f04173fec60d84995cde3b
Rebeca Nonato Silva <38442994+REBECANONATO@users.noreply.github.com>
2018-07-11 16:16:54 -0300

    Update fatorial.py

--- fatorial.py
+++ fatorial.py
@@ -7,3 +7,12 @@ while number > 1:
     number = number - 1
 
 print(product)
+
+# outra maneira
+
+number = 6
+product = number
+
+for i in range(1,number):
+    product = product * i
+print(product)
